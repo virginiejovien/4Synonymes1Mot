a912901d0f7b23a62155fb09f9646b483d661bcd
Virginie Jovien <vjovien@yahoo.fr>
2018-08-25 16:21:21 +0200

    'modifs'

---
+++ data/bdd_jeu_collection_question.js
@@ -0,0 +1,411 @@
+db.question.insert({
+  mot1:"gros",
+  mot2:"important",
+  mot3: "enorme",
+  mot4: "immense",
+  synonyme: "grand"
+},{
+    mot1:"delicieux",
+    mot2:"joli",
+    mot3: "charmant",
+    mot4: "ravissant",
+    synonyme: "adorable"
+},
+{
+  mot1:"insignifiant",
+  mot2:"médiocre",
+  mot3: "mesquin",
+  mot4: "faible",
+  synonyme: "petit"
+},{
+  mot1:"abri",
+  mot2:"appartement",
+  mot3: "asile",
+  mot4: "bâtisse",
+  synonyme: "maison"
+},{
+  mot1:"mauvais",
+  mot2:"inhumain",
+  mot3: "malfaisant",
+  mot4: "détestable",
+  synonyme: "méchant"
+},{
+  mot1:"allée",
+  mot2:"route",
+  mot3: "trajet",
+  mot4: "piste",
+  synonyme: "chemin"
+},{
+  mot1:"diner",
+  mot2:"croquer",
+  mot3: "grignoter",
+  mot4: "boulotter",
+  synonyme: "manger"
+},{
+  mot1:"barque",
+  mot2:"gondole",
+  mot3: "chaloupe",
+  mot4: "yole",
+  synonyme: "bateau"
+},{
+  mot1:"abandonner",
+  mot2:"partir",
+  mot3: "renoncer",
+  mot4: "sortir",
+  synonyme: "quitter"
+},{
+  mot1:"quantité",
+  mot2:"total",
+  mot3: "numéro",
+  mot4: "chiffre",
+  synonyme: "nombre"
+},{
+  mot1:"bécane",
+  mot2:"cycle",
+  mot3: "tandem",
+  mot4: "bicycle",
+  synonyme: "vélo"
+},{
+  mot1:"gosse",
+  mot2:"bambin",
+  mot3: "môme",
+  mot4: "gamin",
+  synonyme: "enfant"
+},{
+  mot1:"tronche",
+  mot2:"frimousse",
+  mot3: "effigie",
+  mot4: "portrait",
+  synonyme: "visage"
+},{
+  mot1:"conte",
+  mot2:"fable",
+  mot3: "récit",
+  mot4: "roman",
+  synonyme: "histoire"
+},{
+  mot1:"végétal",
+  mot2:"plante",
+  mot3: "forêt",
+  mot4: "bois",
+  synonyme: "arbre"
+},{
+  mot1:"panneau",
+  mot2:"terminal",
+  mot3: "paravent",
+  mot4: "protection",
+  synonyme: "écran"
+},{
+  mot1:"activité",
+  mot2:"exercice",
+  mot3: "jeu",
+  mot4: "discipline",
+  synonyme: "sport"
+},{
+  mot1:"asthénie",
+  mot2:"lassitude",
+  mot3: "surmenage",
+  mot4: "ennui",
+  synonyme: "fatigue"
+},{
+  mot1:"matelas",
+  mot2:"plumard",
+  mot3: "couche",
+  mot4: "tapis",
+  synonyme: "lit"
+},{
+  mot1:"congé",
+  mot2:"repos",
+  mot3: "détente",
+  mot4: "pont",
+  synonyme: "vacances"
+},{
+  mot1:"ruisseau",
+  mot2:"affluent",
+  mot3: "fleuve",
+  mot4: "gave",
+  synonyme: "rivière"
+},{
+  mot1:"monceau",
+  mot2:"mont",
+  mot3: "colline",
+  mot4: "tas",
+  synonyme: "montagne"
+},{
+  mot1:"chevalière",
+  mot2:"anneau",
+  mot3: "diamant",
+  mot4: "alliance",
+  synonyme: "bague"
+},{
+  mot1:"basket",
+  mot2:"bottine",
+  mot3: "soulier",
+  mot4: "godillot",
+  synonyme: "chaussure"
+},{
+  mot1:"chaleur",
+  mot2:"ardeur",
+  mot3: "flamme",
+  mot4: "brasier",
+  synonyme: "feu"
+},{
+  mot1:"glacial",
+  mot2:"insensible",
+  mot3: "glacé",
+  mot4: "impassible",
+  synonyme: "froid"
+},{
+  mot1:"clarté",
+  mot2:"lueur",
+  mot3: "éclat",
+  mot4: "jour",
+  synonyme: "lumière"
+},{
+  mot1:"agrume",
+  mot2:"produit",
+  mot3: "pomme",
+  mot4: "baie",
+  synonyme: "fruit"
+},{
+  mot1:"herbe",
+  mot2:"gazon",
+  mot3: "prairie",
+  mot4: "parterre",
+  synonyme: "pelouse"
+},{
+  mot1:"flotte",
+  mot2:"déluge",
+  mot3: "averse",
+  mot4: "crachin",
+  synonyme: "pluie"
+},{
+  mot1:"attraper",
+  mot2:"conquérir",
+  mot3: "vaincre",
+  mot4: "triompher",
+  synonyme: "gagner"
+},{
+  mot1:"miche",
+  mot2:"flûte",
+  mot3: "baguette",
+  mot4: "boule",
+  synonyme: "pain"
+},{
+  mot1:"roquet",
+  mot2:"cabot",
+  mot3: "radin",
+  mot4: "chic",
+  synonyme: "chien"
+},{
+  mot1:"séduction",
+  mot2:"attrait",
+  mot3: "grâce",
+  mot4: "joliesse",
+  synonyme: "charme"
+},{
+  mot1:"compétent",
+  mot2:"expérimenté",
+  mot3: "habile",
+  mot4: "digne",
+  synonyme: "capable"
+},{
+  mot1:"fortuné",
+  mot2:"aisé",
+  mot3: "abondant",
+  mot4: "capitaliste",
+  synonyme: "riche"
+},{
+  mot1:"misérable",
+  mot2:"fauché",
+  mot3: "indigent",
+  mot4: "insolvable",
+  synonyme: "pauvre"
+},{
+  mot1:"action",
+  mot2:"amusement",
+  mot3: "divertissement",
+  mot4: "récréation",
+  synonyme: "jeu"
+},{
+  mot1:"pièces",
+  mot2:"argent",
+  mot3: "espèces",
+  mot4: "numéraire",
+  synonyme: "monnaie"
+},{
+  mot1:"fournil",
+  mot2:"insuccès",
+  mot3: "chaufour",
+  mot4: "échec",
+  synonyme: "four"
+},{
+  mot1:"repas",
+  mot2:"mets",
+  mot3: "nourriture",
+  mot4: "gastronomie",
+  synonyme: "cuisine"
+},{
+  mot1:"mirettes",
+  mot2:"regard",
+  mot3: "vision",
+  mot4: "quinquets",
+  synonyme: "yeux"
+},{
+  mot1:"bec",
+  mot2:"gueule",
+  mot3: "goulot",
+  mot4: "gosier",
+  synonyme: "bouche"
+},{
+  mot1:"guichet",
+  mot2:"cabinet",
+  mot3: "comptoir",
+  mot4: "officine",
+  synonyme: "bureau"
+},{
+  mot1:"seriner",
+  mot2:"fredonner",
+  mot3: "gringotter",
+  mot4: "interpréter",
+  synonyme: "chanter"
+},{
+  mot1:"hausser",
+  mot2:"élever",
+  mot3: "rehausser",
+  mot4: "augmenter",
+  synonyme: "monter"
+},{
+  mot1 : "se lamenter",
+  mot2 : "pleurnicher",
+  mot3 : "geindre",
+  mot4 : "chouiner", 
+  synonyme: "pleurer" 
+},{
+  mot1 : "pellicule",
+  mot2 : "thriller",
+  mot3 : "western", 
+  mot4 : "production", 
+  synonyme : "film" 
+},{
+  mot1 : "tif",
+  mot2 : "persil",
+  mot3 : "poil", 
+  mot4 : "tirage",
+  synonyme : "cheveu" 
+},{
+  mot1 : "gravier",
+  mot2 : "sablon",
+  mot3 : "arène", 
+  mot4 : "beige", 
+  synonyme : "sable" 
+},{ 
+  mot1 : "suspect",
+  mot2 : "douteux", 
+  mot3 : "inquiétant",
+  mot4 : "cuillère",
+  synonyme : "louche" 
+},{
+  mot1 : "canif",
+  mot2 : "poignard",
+  mot3 : "lame",
+  mot4 : "opinel",
+  synonyme : "couteau"
+},{
+  mot1 : "basilique",
+  mot2 : "chapelle",
+  mot3 : "sanctuaire",
+  mot4 : "cathédrale",
+  synonyme : "église" 
+},{
+  mot1 : "hôtellerie",
+  mot2 : "relai",
+  mot3 : "restaurant",
+  mot4 : "taverne", 
+  synonyme : "auberge" 
+},{
+  mot1 : "rame",
+  mot2 : "chemin de fer",
+  mot3 : "train",
+  mot4 : "rer",
+  synonyme : "métro" 
+},{
+  mot1 : "océan",
+  mot2 : "eaux",
+  mot3 : "large",
+  mot4 : "flots",
+  synonyme : "mer"
+},{ 
+  mot1 : "jean",
+  mot2 : "costume",
+  mot3 : "culotte",
+  mot4 : "falzar",
+  synonyme : "pantalon" 
+},{ 
+  mot1 : "gapette", 
+  mot2 : "couvre-chef",
+  mot3 : "képi", 
+  mot4 : "bâche",
+  synonyme : "casquette"
+},{
+  mot1 : "retracer", 
+  mot2 : "exposer",
+  mot3 : "décrire",
+  mot4 : "conter",
+  synonyme : "raconter" 
+},{
+  mot1 : "éteint", 
+  mot2 : "décédé",
+  mot3 : "inerte", 
+  mot4 : "disparition", 
+  synonyme : "mort"
+},{
+  mot1 : "pastille",
+  mot2 : "tablette",
+  mot3 : "cacao", 
+  mot4: "truffe", 
+  synonyme : "chocolat" 
+},{ 
+  mot1 : "match", 
+  mot2 : "compétition",
+  mot3 : "concours", 
+  mot4: "fantasia", 
+  synonyme : "tournoi" 
+},{
+  mot1: "précepteur", 
+  mot2 : "instituteur",
+  mot3 : "enseignant",
+  mot4 : "pédagogue",
+  synonyme : "professeur" 
+},{
+  mot1 : "praticien", 
+  mot2 : "généraliste", 
+  mot3 : "docteur", 
+  mot4 : "clinicien", 
+  synonyme : "médecin" 
+},{ 
+  mot1 : "canine", 
+  mot2 : "aiguille",
+  mot3 : "molaire",
+  mot4 : "croc", 
+  synonyme : "dent" 
+},{ 
+  mot1 : "adolescent", 
+  mot2 : "novice",
+  mot3 : "inexpérimenté",
+  mot4 : "cadet", 
+  synonyme : "jeune"
+},{
+  mot1 : "ancien", 
+  mot2 : "usé", 
+  mot3 : "démodé",
+  mot4 : "antique", 
+  synonyme : "vieux"
+},{ 
+  mot1 : "servir", 
+  mot2 : "assister",
+  mot3 : "épauler", 
+  mot4 : "secourir", 
+  synonyme : "aider" 
+});
+ 

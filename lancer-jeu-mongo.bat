7dc5b933fa0606673d7eebfa267f6a4ba219d538
Virginie Jovien <vjovien@yahoo.fr>
2018-09-15 14:42:19 +0200

    correctifs

---
+++ lancer-jeu-mongo.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start cmd /k C:\devjs\mongodb\server\bin\mongod.exe --dbpath=C:\Users\Virginie\Desktop\WebSocket\4Synonymes1Mot\data
+
+pause
+
+start cmd /k C:\devjs\mongodb\server\bin\mongo.exe
--- lancer-mongo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-start cmd /k C:\devjs\mongodb\server\bin\mongod.exe --dbpath=C:\devjs\mongodb\data\
-
-
-pause
-
-start cmd /k C:\devjs\mongodb\server\bin\mongo.exe

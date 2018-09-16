21c1a9fd75d58736de94c31c71858661b2607fc4
Virginie Jovien <vjovien@yahoo.fr>
2018-09-16 15:24:08 +0200

    base de donnees jeu sur mLab

--- lancer-jeu-mongo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-start cmd /k C:\devjs\mongodb\server\bin\mongod.exe --dbpath=C:\Users\Virginie\Desktop\WebSocket\4Synonymes1Mot\data
-
-pause
-
-start cmd /k C:\devjs\mongodb\server\bin\mongo.exe
